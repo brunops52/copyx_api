469575609d98449fcac4081d67395ec3c6632f77
Bruno Pereira da Silva <bunros52@gmail.com>
2025-08-11 19:25:49 -0300

    feat: creating tweet urls

--- copyx_api/api/urls.py
+++ copyx_api/api/urls.py
@@ -3,10 +3,16 @@ from .views import (
     UserRegistrationView,
     UserLoginView,
     UserProfileView,
-)
+    TweetListCreateView,
+    TweetDetailView,
+    LikeTweetView,
+    )
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('tweets/', TweetListCreateView.as_view(), name='tweet-list'),
+    path('tweets/<int:pk>/', TweetDetailView.as_view(), name='tweet-detail'),
+    path('tweets/<int:pk>like/', LikeTweetView.as_view(), name='tweet-like'),
 ]
